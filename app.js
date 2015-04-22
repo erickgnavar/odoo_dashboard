f8d77e85d599b8b801d85022e8b951620b5a4f09
Erick Navarro <erick@navarro.io>
2015-04-21 23:13:16 -0500

    added customers and sale orders

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ var app = require('express')();
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Odoo = require('node-odoo');
 
 app.use(bodyParser.json());
 
 io.on('connection', function (socket) {
-  console.log('user connected');
+  var odoo = new Odoo({
+    host: 'localhost',
+    port: 8069,
+    database: '',
+    username: '',
+    password: ''
+  });
+
+  odoo.connect(function (err, data) {
+    odoo.search('res.partner', [['customer', '=', true], ['active', '=', true]], function (err, ids) {
+      odoo.get('res.partner', ids, function (err, customers) {
+        io.emit('load_customers', {
+          customers: customers
+        });
+      });
+    });
+  });
 });
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/templates/index.html');
 });
 
-app.post('/send-message/', function (req, res) {
-  io.emit('notification', {
-    message: req.body.message
-  });
+app.post('/new-sale-order/', function (req, res) {
+  io.emit('new_sale_order', req.body);
   res.send({
     status: 'ok'
   });
